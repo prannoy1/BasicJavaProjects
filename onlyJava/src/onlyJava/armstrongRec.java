90e6e9753abf77c50133af3f7d961b5123594a89
prannoy2 <prannoy2@hotmail.com>
2017-04-13 15:31:29 +0530

    arm

---
+++ onlyJava/src/onlyJava/armstrongRec.java
@@ -0,0 +1,22 @@
+package onlyJava;
+
+public class armstrongRec {
+	public static int arm(int number){
+		int x;
+		if(number==1)
+			return 1;
+		else{
+			x = number%10;
+			
+			return (int) (Math.pow(x, 3) + arm(number/10));
+		}
+			
+	}
+	
+	
+	public static void main(String []args){
+		System.out.println(armstrongRec.arm(200));
+		
+	}
+
+}
