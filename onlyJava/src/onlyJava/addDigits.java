8364d789fd3b67ff71ab30cea559ec14389edf7a
prannoy1 <prannoy1@hotmail.com>
2018-11-13 23:38:42 +0530

    comm

--- onlyJava/src/onlyJava/addDigits.java
+++ onlyJava/src/onlyJava/addDigits.java
@@ -1,9 +1,11 @@
 package onlyJava;
-
+//hello
+//praniti here
 public class addDigits {
 	public static void main(String []args){
 		int num = 25589;
 		int sum = 0;
+		int removeme =0;
 		int n;
 		while(num!=0){
 			n=num%10;
